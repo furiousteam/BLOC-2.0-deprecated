357c1810345984b64584869bdea0dc511adedfce
valiant1x <eerie@eerielabs.com>
2017-08-01 00:15:07 -0400

    block time reduced to 120 seconds

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -20,8 +20,8 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 const uint64_t MONEY_SUPPLY									 = UINT64_C(99948151623421337);
-const uint64_t FINAL_SUBSIDY_PER_MINUTE						 = UINT64_C(1740000000);
-const unsigned EMISSION_SPEED_FACTOR                         = 18;
+const uint64_t FINAL_SUBSIDY_PER_MINUTE						 = UINT64_C(2900000000);
+const unsigned EMISSION_SPEED_FACTOR                         = 19;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
@@ -31,7 +31,7 @@ const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
 const uint64_t MINIMUM_FEE                                   = 100000;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = MINIMUM_FEE;
 
-const uint64_t DIFFICULTY_TARGET                             = 200; // seconds
+const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // blocks
 const size_t   DIFFICULTY_CUT                                = 60;  // timestamps to cut after sorting
@@ -63,7 +63,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "intensecoin";
-const char	   GENESIS_COINBASE_TX_HEX[]					 = "013c01ff0001debcd0ac8c0b029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210189e0484b5f4853028a8b3a67330e6a4294673dfe5db3b92de497a60db913d9ed";
+const char	   GENESIS_COINBASE_TX_HEX[]					 = "013c01ff0001af9ea896c605029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101c8a234fc9d37d0b7c5ab0c82ed7993303d153cb5e3a7da8ee8e24d9523902d88";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
