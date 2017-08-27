b8c2ebc77575b12f1efba1c914f3f0d792153aed
valiant1x <eerie@eerielabs.com>
2017-08-27 16:24:37 -0400

    missing files from prev commit

---
+++ external/rocksdb/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h" 
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:Unknown"; 
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Wed 08/23/17 03:31 PM"; 
+const char* rocksdb_build_compile_date = __DATE__; 
