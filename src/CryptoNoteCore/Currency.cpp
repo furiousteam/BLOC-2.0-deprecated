624071a5bdc411b6aad6833f740a46b822f4b5f2
cryptobender69 <35735498+cryptobender69@users.noreply.github.com>
2018-02-09 10:59:05 -0500

    new genesis hex with 8 dec

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -99,7 +99,7 @@ bool Currency::generateGenesisBlock() {
 
 
   // Hard code coinbase tx in genesis block, because through generating tx use random, but genesis should be always the same
-  std::string genesisCoinbaseTxHex = "010a01ff0001808082bf93eff008020ccdce008d79e8a789f1c984fc612fd3d9cde598358c4c12a1c612b1fa550a5b2101a8327fdf74fa176cc36943499fd75501e62b4e8a02ca23cb59e53a75dc57254e";
+  std::string genesisCoinbaseTxHex = "010a01ff000180808d93f5d77102634ea2d46b0bde5e8316f90319ca78becf57eb60e94266dec26b0bcea7e84dfd2101e2ae1a5675066d2bfd41a7daaaeb4189b8644d3cc7c01a6aa4a52e226d5f10c3";
   BinaryArray minerTxBlob;
 
   bool r =
