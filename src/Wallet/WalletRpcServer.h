3c81db8f9d24cd76b728c316b8bd2aac94242cfe
Blockchain-Coin.net <36113405+furiousteam@users.noreply.github.com>
2018-03-03 03:24:42 +0100

    RPC commands now password protected
    Blockchain-Coin is completely secure from the vulnerability described in this article from this version onwards. Technical details are published here: https://www.ayrx.me/cryptonote-unauthenticated-json-rpc
    
    Thanks to @TurtleCoin for the fix

--- src/Wallet/WalletRpcServer.h
+++ src/Wallet/WalletRpcServer.h
@@ -53,6 +53,8 @@ namespace Tools
 
     static const command_line::arg_descriptor<uint16_t> arg_rpc_bind_port;
     static const command_line::arg_descriptor<std::string> arg_rpc_bind_ip;
+		static const command_line::arg_descriptor<std::string> arg_rpc_password;
+		static const command_line::arg_descriptor<bool> arg_rpc_legacy_security;
 
   private:
 
@@ -73,7 +75,9 @@ namespace Tools
     CryptoNote::IWalletLegacy& m_wallet;
     CryptoNote::INode& m_node;
     uint16_t m_port;
+		bool m_legacy;
     std::string m_bind_ip;
+		std::string m_password;
     CryptoNote::Currency& m_currency;
     const std::string m_walletFilename;
 
