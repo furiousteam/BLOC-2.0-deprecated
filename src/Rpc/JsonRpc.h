aef25a8148e3e49762266cb63208e544fe8895cf
Blockchain-Coin.net <36113405+furiousteam@users.noreply.github.com>
2018-03-03 03:23:12 +0100

    RPC commands password protected
    Blockchain-Coin is completely secure from the vulnerability described in this article from this version onwards. Technical details are published here: https://www.ayrx.me/cryptonote-unauthenticated-json-rpc
    
    Thanks to @TurtleCoin for the fix

--- src/Rpc/JsonRpc.h
+++ src/Rpc/JsonRpc.h
@@ -37,6 +37,7 @@ const int errInvalidRequest = -32600;
 const int errMethodNotFound = -32601;
 const int errInvalidParams = -32602;
 const int errInternalError = -32603;
+const int errInvalidPassword = -32604;
 
 class JsonRpcError: public std::exception {
 public:
@@ -62,6 +63,7 @@ public:
 };
 
 typedef boost::optional<Common::JsonValue> OptionalId;
+typedef boost::optional<Common::JsonValue> OptionalPassword;
 
 class JsonRpcRequest {
 public:
@@ -84,6 +86,10 @@ public:
     if (psReq.contains("id")) {
       id = psReq("id");
     }
+
+	 if (psReq.contains("password")) {
+	 	password = psReq("password");
+	 }
 
     return true;
   }
@@ -111,7 +117,12 @@ public:
 
   const OptionalId& getId() const {
     return id;
-  }
+
+}
+  
+  const OptionalPassword& getPassword() const {
+	  return password;
+}
 
   std::string getBody() {
     psReq.set("jsonrpc", std::string("2.0"));
@@ -123,6 +134,7 @@ private:
 
   Common::JsonValue psReq;
   OptionalId id;
+  OptionalPassword password;
   std::string method;
 };
 
