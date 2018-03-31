a5e0756206c1f10324609db6f843caa21b27d7fc
Blockchain-Coin.net <36113405+furiousteam@users.noreply.github.com>
2018-03-31 08:10:12 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -144,8 +144,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[] = "";
 
 const char* const SEED_NODES[] = {
-	"159.65.76.99:2082",
-	"167.99.156.111:2082"
+	"167.99.145.155:2082",
+	"167.99.174.13:2082"
 };
 
 struct CheckpointData {
