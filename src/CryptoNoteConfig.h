362dd228de406ef17435e31a0e6d1ffed0fd3e12
BLOC <36113405+furiousteam@users.noreply.github.com>
2018-07-25 10:28:11 +0200

    Add LWMA-2 + jagerman MTP patch

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -39,17 +39,30 @@ Fees of UINT64_C(1) = 0.0001
 namespace CryptoNote {
 namespace parameters {
 
+const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
+
 const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0xee06ac8; // addresses start with "abLoc"
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 50;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3         = 3 * DIFFICULTY_TARGET;
+
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
+const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 11;
 
 // MONEY_SUPPLY - total number coins to be generated
-const uint64_t MONEY_SUPPLY				     = UINT64_C(500000000000);
-const uint64_t FINAL_SUBSIDY_PER_MINUTE			     = UINT64_C(0);
+const uint64_t MONEY_SUPPLY				                           = UINT64_C(500000000000);
+const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 25000;
+const size_t ZAWY_DIFFICULTY_V2                              = 0;
+const uint8_t ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION       = 3;
+
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 25000;
+const uint64_t DIFFICULTY_WINDOW_V3                          = 60;
+const uint64_t DIFFICULTY_BLOCKS_COUNT_V3                    = DIFFICULTY_WINDOW_V3 + 1;
+
+const uint64_t FINAL_SUBSIDY_PER_MINUTE			                 = UINT64_C(0);
 const unsigned EMISSION_SPEED_FACTOR                         = 21;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -73,16 +86,11 @@ const uint16_t MINIMUM_MIXIN_NO_DUST                         = 0;
 const uint16_t MINIMUM_MIXIN_V1                              = 0;
 const uint16_t MAXIMUM_MIXIN_V1                              = 10;
 
-const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 const size_t   DIFFICULTY_WINDOW                             = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY; // blocks
 const size_t   DIFFICULTY_CUT                                = 60;  // timestamps to cut after sorting
 const size_t   DIFFICULTY_LAG                                = 15;
 static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
-
-const size_t  DIFFICULTY_BLOCKS_V2				= 17;
-const size_t  DIFFICULTY_CUT_V2					= 6;
-const size_t  DIFFICULTY_WINDOW_V2				= DIFFICULTY_BLOCKS_V2 + DIFFICULTY_CUT_V2 * 2;
 
 const size_t   MAX_BLOCK_SIZE_INITIAL                        	= 20 * 1024;
 const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_NUMERATOR         	= 100 * 1024;
@@ -103,8 +111,9 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              	= 4;
 
 const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                	= 0;
 const uint32_t UPGRADE_HEIGHT_V2                             	= 50;
-const uint32_t UPGRADE_HEIGHT_MAX_BLOCK_SIZE			= 65000;
+const uint32_t UPGRADE_HEIGHT_MAX_BLOCK_SIZE			            = 65000;
 const uint32_t UPGRADE_HEIGHT_V3                             	= 100;
+const uint32_t UPGRADE_HEIGHT_V4                             =  50000;
 const unsigned UPGRADE_VOTING_THRESHOLD                      	= 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         	= EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t UPGRADE_WINDOW                                	= EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
@@ -126,6 +135,7 @@ const uint8_t  CURRENT_TRANSACTION_VERSION                   =  TRANSACTION_VERS
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_2                         =  2;
 const uint8_t  BLOCK_MAJOR_VERSION_3                         =  3;
+const uint8_t  BLOCK_MAJOR_VERSION_4                         =  4;
 const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
 const uint8_t  BLOCK_MINOR_VERSION_1                         =  0;
 
